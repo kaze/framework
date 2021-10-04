e5a7814622e022a43ededd7c466e90e551adf210
Krisztian Gulyas <krisztian.gulyas@flexiana.com>
2021-10-04 15:23:54 +0200

    Add config logger interceptor

--- src/xiana/message.clj
+++ src/xiana/message.clj
@@ -1,19 +1,27 @@
 (ns xiana.message
   (:require
     [clojure.pprint :refer [pprint]]
-    [xiana.core :as xiana]))
+    [xiana.config :as config]
+    [xiana.core :refer [ok]]))
 
 (def log
   "Log interceptor.
-  Enter: Print 'Enter:' followed by the complete state map.
-  Leave: Print 'Leave:' followed by the complete state map."
-  {:enter (fn [state] (pprint ["Enter: " state]) (xiana/ok state))
-   :leave (fn [state] (pprint ["Leave: " state]) (xiana/ok state))})
+  Enter: Print 'Enter:' followed by the complete ctx map.
+  Leave: Print 'Leave:' followed by the complete ctx map."
+  {:enter (fn [ctx] (pprint ["Enter: " ctx]) (ok ctx))
+   :leave (fn [ctx] (pprint ["Leave: " ctx]) (ok ctx))})
 
 (defn message
   "This interceptor creates a function that prints predefined message.
   Enter: Print an arbitrary message.
   Leave: Print an arbitrary message."
   [msg]
-  {:enter (fn [state] (println msg) (xiana/ok state))
-   :leave (fn [state] (println msg) (xiana/ok state))})
+  {:enter (fn [ctx] (println msg) (ok ctx))
+   :leave (fn [ctx] (println msg) (ok ctx))})
+
+(def config
+  "Config log interceptor.
+  Enter: Print 'Enter:' followed by the complete config map.
+  Leave: Print 'Leave:' followed by the complete config map."
+  {:enter (fn [ctx] (pprint ["Enter: " @config/-config]) (ok ctx))
+   :leave (fn [ctx] (pprint ["Leave: " @config/-config]) (ok ctx))})
